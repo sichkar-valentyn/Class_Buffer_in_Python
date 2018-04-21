a1e2734558c4132dd9a498e18c3a8a0206049efa
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-04-21 17:59:49 +0300

    Add files via upload

---
+++ Class_Buffer_in_Python.py
@@ -0,0 +1,39 @@
+# Implementing the task
+# Creating class Buffer
+# Adding to the buffer numbers
+# Saving in the buffer only < 5 numbers
+# Printing the sum of sequence of 5 numbers
+
+# Creating the class Buffer:
+class Buffer:
+    # Constructor of the class
+    def __init__(self):
+        # Creating list to store the current sequence but not longer then 5 elements
+        self.lst = []
+          
+    # Adding the sequence of numbers:
+    def add(self, *args):
+        # Going through all elements of the tuple *args
+        for i in range(len(args)):
+            # Adding elements to the list till it has 5
+            if len(self.lst) < 5:
+                self.lst += [args[i]]
+            # Printing sum of the 5 elements and clearing the list
+            if len(self.lst) == 5:
+                print(sum(self.lst))
+                self.lst = []
+        
+    # Returning the currently saved elements of the sequence
+    def get_current_part(self):
+        return self.lst
+
+# Showing the results
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part()  # show [1, 2, 3]
+buf.add(4, 5, 6)  # print 15 
+buf.get_current_part()  # Show [6]
+buf.add(7, 8, 9, 10)  # print(40)
+buf.get_current_part()  # Show []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)  # print 5 and again print 5
+buf.get_current_part()  # Show [1]
